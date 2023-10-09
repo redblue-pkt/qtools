7a3cace3a42d11e62b5777d71744b0c0d7d29175
Giammarco <stich86@gmail.com>
2023-10-09 15:24:36 +0200

    Update mibibsplit.c

--- mibibsplit.c
+++ mibibsplit.c
@@ -11,35 +11,35 @@ unsigned int rflag=0,wflag=0,padr=0;
 unsigned int sig1,sig2;
 
 if (argc != 2) {
-  printf("\n Не указано имя файла MIBIB\n");
+  printf("\n MIBIB File name not specified\n");
   return;
 }
 in=fopen(argv[1],"rb");
 if (in == 0) {
-  printf("\n Ошибка открытия файла MIBIB\n");
+  printf("\n Error opening file MIBIB\n");
   return;
 }  
 
 
-// ищем начало блока таблиц разделов
+// looking for the beginning of the partition table block
 while (!feof(in)) {
  fread(&sig1,1,4,in);  
  fread(&sig2,1,4,in);  
- if ((sig1 == 0xfe569fac) && (sig2 == 0xcd7f127a)) break; // найдена сигнатура блока таблиц разделов
- fseek(in,504,SEEK_CUR);  // пропускаем остаток сектора
+ if ((sig1 == 0xfe569fac) && (sig2 == 0xcd7f127a)) break; // partition table block signature found
+ fseek(in,504,SEEK_CUR);  // skip the rest of the sector
  padr+=512;
 } 
 if (feof(in)) {
-   printf("\n Сигнатура блока таблиц разделов не найдена\n");
+   printf("\n Partition table block signature not found\n");
    fclose(in);
    return;
 }
-// теперь padr - адрес блока разделов внутри файла
-// читаем в буфер весь MIBIB до этого адреса - это будет SBL1 с хвостом
+// padr - is the address of the section block inside the file
+// read the entire MIBIB up to this address into the buffer - this will be SBL1 with a tail
 fseek(in,0,SEEK_SET);
 fread(buf,padr,1,in);  
 
-// ищем реальный хвост раздела
+// looking for the real tail of the section
 for(i=padr-1;i>0;i--) {
   if (buf[i] != 0xff) break;
 }
@@ -47,42 +47,42 @@ out=fopen("sbl1.bin","wb");
 fwrite(buf,i+1,1,out);
 fclose (out);
   
-// Ищем сигнатуры таблиц разделов
+// Looking for partition table signatures
 fseek(in,padr,SEEK_SET);
 while(!feof(in)) {
  fread(&sig1,1,4,in);  
  fread(&sig2,1,4,in);  
  if ((!rflag) && (sig1 == 0x55ee73aa) && (sig2 == 0xe35ebddb)) {
-   // найдена таблица чтения
-   fseek(in,-8,SEEK_CUR); // отъезжаем на начало сигнатуры
+   // reading table found
+   fseek(in,-8,SEEK_CUR); // we move to the beginning of the signature
    fread(buf,2048,1,in);
-   npar=*((unsigned int*)&buf[12]); // число разделов
+   npar=*((unsigned int*)&buf[12]); // number of sections
    out=fopen("ptable-r.bin","wb");
    fwrite(buf,16+28*npar,1,out);
    fclose (out);
-   rflag=1;  // таблица чтения у нас есть
-   continue; // пропускаем дальнейшую обработку сектора
+   rflag=1;  // we have a reading table
+   continue; // skip further processing of the sector
  }
  
  if ((!wflag) && (sig1 == 0xaa7d1b9a) && (sig2 == 0x1f7d48bc)) {
-   // найдена таблица записи
-   fseek(in,-8,SEEK_CUR); // отъезжаем на начало сигнатуры
+   // record table found
+   fseek(in,-8,SEEK_CUR); // we move to the beginning of the signature
    fread(buf,2048,1,in);
-   npar=*((unsigned int*)&buf[12]); // число разделов
+   npar=*((unsigned int*)&buf[12]); // number of sections
    out=fopen("ptable-w.bin","wb");
    fwrite(buf,16+28*npar,1,out);
    fclose (out);
-   wflag=1;  // таблица чтения у нас есть
-   continue; // пропускаем дальнейшую обработку сектора
+   wflag=1;  // we have a reading table
+   continue; // skip further processing of the sector
  }
  if (rflag && wflag)
  {
      fclose(in);
-     return;   // найдены обе таблицы
+     return;   // both tables found
  }
- fseek(in,504,SEEK_CUR);  // пропускаем остаток сектора
+ fseek(in,504,SEEK_CUR);  // skip the rest of the sector
 }  
-if (!rflag) printf("\n Таблица чтения не найдена\n");
-if (!wflag) printf("\n Таблица записи не найдена\n");
+if (!rflag) printf("\n Read table not found\n");
+if (!wflag) printf("\n Write table not found\n");
     fclose(in);
 }
