b0c8fb934c96c9a3e97efe6f6b8b3358ee08de60
Giammarco <stich86@gmail.com>
2023-10-09 15:16:35 +0200

    Update iowmem.asm

--- iowmem.asm
+++ iowmem.asm
@@ -1,28 +1,28 @@
-@  Апплет записи данных в память модема через команду 11
+@ Applet for writing data to modem memory using command 0x11
 @
-@ На входе:
-@ R0 = адрес начала данного кода (точки start)
-@ R1 = адрес ответного буфера
+@ Input:
+@ R0 = start address of this code (label start)
+@ R1 = address of the response buffer
 @
 
-      .org    0	
-      .byte   0,0,0x11,0      @ код команды 11 и выравнивающие байты
+.org    0   
+.byte   0,0,0x11,0   @ command code 0x11 and alignment bytes
 start: 
-       ADD    R0,R0,#(param-start) @ адрес, откуда начинаются данные для записи
-       LDR    R3,dstadr           @ адрес, куда их пихать в модеме
-       LDR    R4,lenadr           @ размер записываемых данных в байтах
-       ADD    R4,R3,R4            @ адрес конца блока данных
+    ADD    R0, R0, #(param - start) @ address where data for writing begins
+    LDR    R3, dstadr              @ address where data should be written in the modem
+    LDR    R4, lenadr              @ size of the data to be written in bytes
+    ADD    R4, R3, R4              @ end address of the data block
 
 locloop:   
-       LDR    R2,[R0],#4	  
-       STR    R2,[R3],#4
-       CMP    R3,R4
-       BCC    locloop
-       MOV    R0,#0x12         @ код ответа 
-       STRB   R0,[R1]          @ сохраняем его в ответный буфер
-       MOV    R4,#1            @ размер ответа
-       BX     LR
+    LDR    R2, [R0], #4    
+    STR    R2, [R3], #4
+    CMP    R3, R4
+    BCC    locloop
+    MOV    R0, #0x12          @ response code 
+    STRB   R0, [R1]           @ save it in the response buffer
+    MOV    R4, #1             @ response size
+    BX     LR
 
-dstadr:.word  0
-lenadr:.word  0
-param:       
+dstadr: .word 0
+lenadr: .word 0
+param:
