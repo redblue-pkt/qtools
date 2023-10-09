c26601ca5cce84e3d8e2aee98ccc8ec1da76b3a8
Giammarco <stich86@gmail.com>
2023-10-09 14:58:07 +0200

    Update dl8200.sh

--- dl8200.sh
+++ dl8200.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# Скрипт для заливки загрузчика в модем, находящийся в стандартном режиме
+# Script for loading the bootloader into a modem in standard mode
 #
-# Параметр скрипта - имя порта tty, по умолчанию - /dev/ttyUSB0
+# Script parameter - tty port name, default - /dev/ttyUSB0
 #
 PORT=$1
 if [ -z "$PORT" ]; then PORT=/dev/ttyUSB0; fi
@@ -13,30 +13,30 @@ if [ -z "$LDR" ]; then LDR=loaders/NPRG8200p.bin; fi
 
 echo Diagnostic port: $PORT
 
-# Ждем появления порта в системе
+# We are waiting for the port to appear in the system
 while [ ! -c $PORT ]
  do
   sleep 1
  done
 
-# команда переключения в download mode
+# command to switch to download mode
 echo entering download mode...
 ./qcommand -p $PORT -c "c b" >/dev/null
 
-# Ждем пропадания порта из системы
+# We are waiting for the port to disappear from the system
 while [ -c $PORT ]
  do
   true
  done
 echo diagnostic port removed
 
-# Ждем появления нового порта, уже download
+# We are waiting for the new port to appear, download already
 while [ ! -c $PORT ]
  do
   sleep 1
  done
 
-# Заливаем загрузчик
+# Loading the bootloader
 echo download mode entered
 sleep 2
 ./qdload -p $PORT  -i -t -k1 -d20 -a100000 $LDR
